aa4b9f70a1e02cea2eb061d448354e91f4b546d7
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-24 19:58:03 -0600

    define entities

---
+++ app/SDK/Entities/Client.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\SDK;
+
+class Client
+{
+    private int $id;
+    private string $name;
+    private string $hash;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getHash(): string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): void
+    {
+        $this->hash = $hash;
+    }
+}
+
---
+++ app/SDK/Entities/Thread.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\SDK;
+
+class Thread
+{
+    private int $id;
+    private int $clientId;
+
+    public function __construct(int $id, int $clientId)
+    {
+        $this->id = $id;
+        $this->clientId = $clientId;
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getClientId(): int
+    {
+        return $this->clientId;
+    }
+
+    public function setClientId(int $clientId): void
+    {
+        $this->clientId = $clientId;
+    }
+}
