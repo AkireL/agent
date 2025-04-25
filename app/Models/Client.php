94c5adc7273d005c2496fc2037435d3de58e6174
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-24 19:57:37 -0600

    define models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,4 +10,9 @@ class Client extends Model
         'name',
         'hash',
     ];
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    protected $fillable = [
+        'client_id',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
