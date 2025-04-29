0f6a3f51bd14883119300b294e5ec7d0d774a410
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-28 19:46:16 -0600

    get messages

--- app/SDK/Infrastructure/EloquentMessage.php
+++ app/SDK/Infrastructure/EloquentMessage.php
@@ -18,9 +18,34 @@ class EloquentMessage implements \App\SDK\Ports\MessageRepositoryInterface
 
         return new Message(
             role: $message->role,
-            content: json_decode($message->content, true),
+            content: $message->content,
             threadId: $message->thread_id
         );
     }
 
+    public function listMessages(int $threadId): array
+    {
+        $messages = \App\Models\Message::where('thread_id', $threadId)->get();
+
+        return $messages->map(function ($message) {
+            $data = [];
+            $content = $message->content;
+
+            $data['role'] = $message->role;
+
+            if (array_key_exists('content', $content)) {
+                $data['content'] = $content['content'];
+
+                return $data;
+            }
+
+            if (array_key_exists('tool_call', $content)) {
+                $data['tool_call'] = $content['tool_call'];
+                return $data;
+            }
+            $data['content'] = $content;
+
+            return $data;
+        })->toArray();
+    }
 }
