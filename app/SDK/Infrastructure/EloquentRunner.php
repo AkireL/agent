37d5121348cdd0aa7fdb7e23eb1838aa5dec6913
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-28 19:46:41 -0600

    added eloquentRunner

---
+++ app/SDK/Infrastructure/EloquentRunner.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\SDK\Infrastructure;
+
+use App\Models\Runner;
+
+class EloquentRunner implements \App\SDK\Ports\RunnerRepositoryInterface
+{
+    private function create($threadId)
+    {
+        return Runner::create([
+            'thread_id' => $threadId,
+        ]);
+    }
+
+    private function find($threadId, $runnerId)
+    {
+        return Runner::where([
+            'thread_id'=> $threadId,
+            'id' => $runnerId,
+        ])->first();
+    }
+
+    public function getOrCreate(int $threadId, ?int $runnerId=null): int
+    {
+        if ($threadId) {
+            $runner = $this->find($threadId, $runnerId);
+
+            if (! $runner) {
+                $runner = $this->create($threadId);
+            }
+
+            return $runner->id;
+        }
+
+        $thread = \App\Models\Thread::find($threadId);
+
+        $runner = $thread->runners()->create();
+
+        return $runner->id;
+    }
+
+}
