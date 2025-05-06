f878fddd8cd2219b52ac606113ec19fa7ff5ccef
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-05-05 19:59:53 -0600

    refactor constructor and create method to include model, system prompt, and tools

--- app/SDK/Services/Ollama/Chat.php
+++ app/SDK/Services/Ollama/Chat.php
@@ -7,14 +7,34 @@ class Chat
     private OllamaClient $ollamaClient;
     private string $model;
     private string $systemPrompt;
+    private array $tools;
 
-    public function __construct(OllamaClient $ollamaClient)
+    public function __construct(OllamaClient $ollamaClient, string $model, string $systemPrompt = '', array $tools)
     {
+        $this->model = $model;
+        $this->systemPrompt = $systemPrompt;
+        $this->tools = $tools;
         $this->ollamaClient = $ollamaClient;
     }
 
-    public function create(array $parameters)
+    public function create(array $messages)
     {
+        $parameters = [];
+
+        $parameters['model'] = $this->model;
+
+        if (! empty($this->tools)) {
+            $parameters['tools'] = $this->tools;
+        }
+
+        $parameters["messages"] = array_merge([
+            [
+                'role' => "system",
+                'content' => $this->systemPrompt,
+            ],
+        ],
+        $messages);
+
         return $this->ollamaClient->post('chat', $parameters, false);
     }
 }
