5ad0c04c293627f95f72aea5305a3197496d3f7b
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-05-05 19:55:16 -0600

    create methos to set values

--- app/SDK/Services/Ollama/Ollama.php
+++ app/SDK/Services/Ollama/Ollama.php
@@ -5,20 +5,46 @@ namespace App\SDK\Services\Ollama;
 class Ollama implements \App\SDK\Ports\LLMInterface
 {
     private OllamaClient $ollamaClient;
-
-    public static function client(string $host = 'http://localhost:11434'): Ollama
-    {
-        return new self($host);
-    }
+    private string $model = 'qwen3';
+    private string $systemPrompt;
+    private array $tools = [];
 
     private function __construct(string $host)
     {
         $this->ollamaClient = new OllamaClient($host);
     }
 
+    public static function client(string $host = 'host.docker.internal:11434'): Ollama
+    {
+        return new self($host);
+    }
+
+    public function setModel($model): self
+    {
+        $this->model = $model;
+        return $this;
+    }
+
+    public function setSystemPrompt($systemPrompt): self
+    {
+        $this->systemPrompt = $systemPrompt;
+        return $this;
+    }
+
+    public function setTools(array $tools): self
+    {
+        $this->tools = $tools;
+        return $this;
+    }
+
     public function chat()
     {
-        return new Chat($this->ollamaClient);
+        return new Chat(
+            $this->ollamaClient,
+            $this->model,
+            $this->systemPrompt,
+            $this->tools
+        );
     }
 
 }
